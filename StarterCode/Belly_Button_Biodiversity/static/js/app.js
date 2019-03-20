a8c86426698c49d24afdb69965a5d5ed200e7734
leetaylor7 <44614628+leetaylor7@users.noreply.github.com>
2019-03-20 18:40:52 -0500

    Add files via upload
    Added the pie chart

--- StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -4,10 +4,9 @@ function buildMetadata(sample) {
   
 
   // Use `d3.json` to fetch the metadata for a sample
-  var url = `/metadata/${sample}`;
+  var url = `/metadata/${sample}`
   d3.json(url).then(function(response) {
     console.log(Object.entries(response))
-    
 
     // Use d3 to select the panel with id of `#sample-metadata`
     var metaData = d3.select("#sample-metadata");
@@ -19,8 +18,6 @@ function buildMetadata(sample) {
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
-
-
 
 
 
@@ -58,6 +55,20 @@ function buildCharts(sample) {
    // @TODO: Build a Pie Chart
    // HINT: You will need to use slice() to grab the top 10 sample_values,
    // otu_ids, and labels (10 each).
+   trace2 = {
+     type: "pie",
+     labels: response.otu_ids.slice(0,10),
+     values: response.sample_values.slice(0,10),
+     text: response.otu_labels.slice(0,10)    
+   };
+
+   var data2 = [trace2];
+
+   var layout2 ={
+     title: "Pie Chart"
+   }
+
+   Plotly.newPlot("pie", data2, layout2)
 });
 }
 
@@ -77,14 +88,14 @@ function init() {
     // Use the first sample from the list to build the initial plots
     const firstSample = sampleNames[0];
     buildCharts(firstSample);
-    buildMetadata(firstSample);
+    // buildMetadata(firstSample);
   });
 }
 
 function optionChanged(newSample) {
   // Fetch new data each time a new sample is selected
-  // buildCharts(newSample);
-  // buildMetadata(newSample);
+  buildCharts(newSample);
+  buildMetadata(newSample);
 }
 
 // Initialize the dashboard
